0736717d176a4a5187ece998f876e511625bf2f0
alixlab <alicelabarca@gmail.com>
2019-03-01 15:23:03 -0300

    Versão 0.0.1, atualizações no js e html

--- src/main.js
+++ src/main.js
@@ -1,13 +1,44 @@
-for (let item in INJURIES){
-    let obj = INJURIES[item];
-    if (parseInt(obj["Year"].split("-")[0]) > 2005 && parseInt(obj["Year"].split("-")[0]) < 2016) {
-	    let dataValue = document.createElement("p");
-        dataValue.textContent = obj["Year"].split("-")[0] + "\t" + 
-                           obj["Total_Injured_Persons_Pedestrians"] + "\t" +
-                           obj["Total_Injured_Persons_Bus_Occupants"] + "\t" +
-                           obj["Total_Injured_Persons_Motorcyclists"] + "\t" +
-                           obj["Total_Injured_Persons_Pedalcyclists"] + "\t" +
-                           obj["Total_Injured_Persons_Passenger_Car_Occupants"];
-        document.body.appendChild(dataValue); 
-	}
-}
+function getData() {
+        let dataset = [["Year", 
+                        "Pedestrians",
+                        "Bus Occupants",
+                        "Motorcyclists",
+                        "Pedalcyclists", 
+                        "Passenger Car Occupants"]];
+        for (obj of INJURIES){
+          if (parseInt(obj["Year"].split("-")[0]) > 2005 && parseInt(obj["Year"].split("-")[0]) < 2016 ) {
+            let record = [];
+            record.push(obj["Year"].split("-")[0]);
+            record.push(obj["Total_Injured_Persons_Pedestrians"]);
+            record.push(obj["Total_Injured_Persons_Bus_Occupants"]);
+            record.push(obj["Total_Injured_Persons_Motorcyclists"]);
+            record.push(obj["Total_Injured_Persons_Pedalcyclists"]);
+            record.push(obj["Total_Injured_Persons_Passenger_Car_Occupants"]);
+            dataset.push(record);
+          }
+        }
+        return dataset
+      }
+      
+      function tableCreate(dataset) {
+        let body = document.getElementById("tables-div");
+        let tbl = document.createElement('table');
+        tbl.style.width = '100%';
+        tbl.setAttribute('border', '1');
+        let tbdy = document.createElement('tbody');
+        for (row of dataset) {
+          let tr = document.createElement('tr');
+          for (column of row) {
+              let td = document.createElement('td');
+              td.appendChild(document.createTextNode(column))
+              tr.appendChild(td)
+            }
+          tbdy.appendChild(tr);
+        }
+        tbl.appendChild(tbdy);
+        body.appendChild(tbl)
+      }
+      
+      let dataset = getData();
+      tableCreate(dataset);
+      
