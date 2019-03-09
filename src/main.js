99d7f55926961c4e5a88c268c0326de24f6c3187
alixlab <alicelabarca@gmail.com>
2019-03-09 10:04:46 -0300

    Função ordenar no js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ let endSelect = document.getElementById("yearFilterEnd");
 let end = parseInt(endSelect.options[endSelect.selectedIndex].value);
 let categorySelect = document.getElementById("category");
 let category = categorySelect.options[categorySelect.selectedIndex].value;
+let orderSelect = document.getElementById("orderList");
+let order = orderSelect.options[orderSelect.selectedIndex].value;
 let btnFilter = document.getElementById("btnSelect");
 btnFilter.addEventListener("click", filterData);
 let dataset = getData(start, end, category);
+sortByYear(dataset, order);
 tableCreate(dataset);
 
 function getData(start, end, category) {
@@ -25,49 +28,62 @@ function getData(start, end, category) {
       let record = [];
       record.push(year);
       if (obj[category] !== null) {
-        record.push(obj[category].toLocaleString("pt-BR")); 
+        record.push(obj[category].toLocaleString("pt-BR"));
       } else {
         record.push("Não disponível");
       }
-        dataset.push(record);
-      }
+      dataset.push(record);
     }
-    return dataset
   }
+  return dataset
+}
 
-  function tableCreate(dataset) {
-    let body = document.getElementById("tables-div");
-    let tbl = document.createElement('table');
-    tbl.setAttribute("id", "table");
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    let tbdy = document.createElement('tbody');
-    for (row of dataset) {
-      let tr = document.createElement('tr');
-      for (column of row) {
-        let td = document.createElement('td');
-        td.appendChild(document.createTextNode(column))
-        tr.appendChild(td)
-      }
-      tbdy.appendChild(tr);
+function tableCreate(dataset) {
+  let body = document.getElementById("tables-div");
+  let tbl = document.createElement('table');
+  tbl.setAttribute("id", "table");
+  tbl.style.width = '100%';
+  tbl.setAttribute('border', '1');
+  let tbdy = document.createElement('tbody');
+  for (row of dataset) {
+    let tr = document.createElement('tr');
+    for (column of row) {
+      let td = document.createElement('td');
+      td.appendChild(document.createTextNode(column))
+      tr.appendChild(td)
     }
-    tbl.appendChild(tbdy);
-    body.appendChild(tbl)
+    tbdy.appendChild(tr);
   }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
+}
 
-  function filterData() {
-    removeTable();
-    let startSelect = document.getElementById("yearFilterBegin");
-    let start = parseInt(startSelect.options[startSelect.selectedIndex].value);
-    let endSelect = document.getElementById("yearFilterEnd");
-    let end = parseInt(endSelect.options[endSelect.selectedIndex].value);
-    let categorySelect = document.getElementById("category");
-    let category = categorySelect.options[categorySelect.selectedIndex].value;
-    let dataset = getData(start, end, category);
-    tableCreate(dataset);
-  }
+function filterData() {
+  removeTable();
+  let startSelect = document.getElementById("yearFilterBegin");
+  let start = parseInt(startSelect.options[startSelect.selectedIndex].value);
+  let endSelect = document.getElementById("yearFilterEnd");
+  let end = parseInt(endSelect.options[endSelect.selectedIndex].value);
+  let categorySelect = document.getElementById("category");
+  let category = categorySelect.options[categorySelect.selectedIndex].value;
+  let orderSelect = document.getElementById("orderList");
+  let order = orderSelect.options[orderSelect.selectedIndex].value;
+  let dataset = getData(start, end, category);
+  sortByYear(dataset, order);
+  tableCreate(dataset);
+}
 
-  function removeTable() {
-    let table = document.getElementById("table");
-    table.parentNode.removeChild(table);
-  }
+function removeTable() {
+  let table = document.getElementById("table");
+  table.parentNode.removeChild(table);
+}
+
+function sortByYear(dataset, order) {
+  dataset.sort(function (a, b) {
+    if (order === "asc") {
+      return a[0] - b[0];
+    } else {
+      return b[0] - a[0];
+    }
+  });
+}
