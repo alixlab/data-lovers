c66fb5984ea8a3d188c6aac8b0374557546c9657
caroldrumond <caroldrumondpro@gmail.com>
2019-03-10 12:55:00 -0300

    paginas

--- src/main.js
+++ src/main.js
@@ -203,8 +203,504 @@ function draw96Chart() {
 google.charts.setOnLoadCallback(draw96Chart);
 
 
-// document.getElementById('year96').addEventListener('click', function(){
-//   window.open("src\year96.html", "_self")
-// })
+function draw97Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[1][0]],
+    ['Moto', allData[1][1]],
+    ['Carro', allData[1][2]],
+    ['Bicicleta', allData[1][3]],
+    ['Pedestre', allData[1][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 1997',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes97'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw97Chart);
+
+function draw98Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[2][0]],
+    ['Moto', allData[2][1]],
+    ['Carro', allData[2][2]],
+    ['Bicicleta', allData[2][3]],
+    ['Pedestre', allData[2][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 1998',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes98'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw98Chart);
+
+function draw99Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[3][0]],
+    ['Moto', allData[3][1]],
+    ['Carro', allData[3][2]],
+    ['Bicicleta', allData[3][3]],
+    ['Pedestre', allData[3][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 1999',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes99'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw99Chart);
+
+function draw00Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[4][0]],
+    ['Moto', allData[4][1]],
+    ['Carro', allData[4][2]],
+    ['Bicicleta', allData[4][3]],
+    ['Pedestre', allData[4][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2000',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes00'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw00Chart);
+
+function draw01Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[5][0]],
+    ['Moto', allData[5][1]],
+    ['Carro', allData[5][2]],
+    ['Bicicleta', allData[5][3]],
+    ['Pedestre', allData[5][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2001',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes01'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw01Chart);
+
+function draw02Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[6][0]],
+    ['Moto', allData[6][1]],
+    ['Carro', allData[6][2]],
+    ['Bicicleta', allData[6][3]],
+    ['Pedestre', allData[6][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2002',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes02'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw02Chart);
+
+function draw03Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[7][0]],
+    ['Moto', allData[7][1]],
+    ['Carro', allData[7][2]],
+    ['Bicicleta', allData[7][3]],
+    ['Pedestre', allData[7][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2003',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes03'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw03Chart);
+
+function draw04Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[8][0]],
+    ['Moto', allData[8][1]],
+    ['Carro', allData[8][2]],
+    ['Bicicleta', allData[8][3]],
+    ['Pedestre', allData[8][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2004',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes04'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw04Chart);
+
+function draw05Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[9][0]],
+    ['Moto', allData[9][1]],
+    ['Carro', allData[9][2]],
+    ['Bicicleta', allData[9][3]],
+    ['Pedestre', allData[9][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2005',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes05'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw05Chart);
+
+function draw06Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[10][0]],
+    ['Moto', allData[10][1]],
+    ['Carro', allData[10][2]],
+    ['Bicicleta', allData[10][3]],
+    ['Pedestre', allData[10][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2006',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes06'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw06Chart);
+
+function draw07Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[11][0]],
+    ['Moto', allData[11][1]],
+    ['Carro', allData[11][2]],
+    ['Bicicleta', allData[11][3]],
+    ['Pedestre', allData[11][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2007',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes07'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw07Chart);
+
+function draw08Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[12][0]],
+    ['Moto', allData[12][1]],
+    ['Carro', allData[12][2]],
+    ['Bicicleta', allData[12][3]],
+    ['Pedestre', allData[12][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2008',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes08'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw08Chart);
+
+function draw09Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[13][0]],
+    ['Moto', allData[13][1]],
+    ['Carro', allData[13][2]],
+    ['Bicicleta', allData[13][3]],
+    ['Pedestre', allData[13][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2009',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes09'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw09Chart);
+
+function draw10Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[14][0]],
+    ['Moto', allData[14][1]],
+    ['Carro', allData[14][2]],
+    ['Bicicleta', allData[14][3]],
+    ['Pedestre', allData[14][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2010',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes10'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw10Chart);
+
+function draw11Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[15][0]],
+    ['Moto', allData[15][1]],
+    ['Carro', allData[15][2]],
+    ['Bicicleta', allData[15][3]],
+    ['Pedestre', allData[15][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2011',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes11'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw11Chart);
+
+function draw12Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[16][0]],
+    ['Moto', allData[16][1]],
+    ['Carro', allData[16][2]],
+    ['Bicicleta', allData[16][3]],
+    ['Pedestre', allData[16][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2012',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes12'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw12Chart);
+
+function draw13Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[17][0]],
+    ['Moto', allData[17][1]],
+    ['Carro', allData[17][2]],
+    ['Bicicleta', allData[17][3]],
+    ['Pedestre', allData[17][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2013',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes13'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw13Chart);
+
+function draw14Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[18][0]],
+    ['Moto', allData[18][1]],
+    ['Carro', allData[18][2]],
+    ['Bicicleta', allData[18][3]],
+    ['Pedestre', allData[18][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2014',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes14'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw14Chart);
+
+function draw15Chart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Tipo de transporte', 'Pessoas Feridas'],
+    ['Onibus', allData[19][0]],
+    ['Moto', allData[19][1]],
+    ['Carro', allData[19][2]],
+    ['Bicicleta', allData[19][3]],
+    ['Pedestre', allData[19][4]]
+  ]);
+
+  var options = {
+    title: 'Acidentes de Transito em 2015',
+    backgroundColor: 'transparent',
+    is3D: true,
+    legend: 'labeled',
+    width: 400,
+    height: 200,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('allTypes15'));
+
+  chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(draw15Chart);
+
+
+
+
+
+
 
 console.log('Ao vivo direto de Dubai - BILLY, Willie')
